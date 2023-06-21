272e0d5642b9bdcc8f13b5e28de7f3634083fcf1
francesco-p <pelosinf@gmail.com>
2023-06-21 22:57:55 +0200

    ok

--- timmviz.py
+++ timmviz.py
@@ -1,95 +1,119 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import torch
 import timm
-import torchshow as ts
-from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, ColumnsAutoSizeMode
 import  streamlit_toggle as tog
-from utils import process_attrs, show_hist, model_size
-from torchvision import datasets, transforms
-from einops import rearrange
+#from utils import model_size
 import os
 from PIL import Image
-import torch.nn.functional as F
-import builtins
 import urllib
-import seaborn as sns
 import torchvision.transforms.functional as transform
 import cv2
-#sns.set()
+import pickle as pkl
+from collections import defaultdict
+import cmapy
+import pandas as pd
 
-# deep lab → senza la parte di (decoder) e viceversa
-# saliency detection (unsupervised)
-# metodi visualizzazione 
+def model_size(model):
+    """ Prints model info """
+    params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    mb = (params * 32) / 2**23
+    gb = (params * 32) / 2**33
+    #return params, mb, gb
+    dictionary = {'Params': [params], 'MB': [f'{mb:.2f}'], 'GB': [f'{gb:.2f}']}
+    return pd.DataFrame(dictionary)
+
+def to_255(x):
+    return (x - x.min()) / (x.max() - x.min()) * 255
 
 @st.cache_data
-def prepare_feature_maps(_model, model_name, _img, img_name, pretrained):
+def prepare_feature_maps(_model, _img, fname):
+    """Prepare feature maps for visualization
+    Args:
+        _model (torch.nn.Module): model 
+        _img (torch.Tensor): image
+        fname (str): filename to save feature maps
+    Returns:
+        None
+    """
+
     model = _model
+    img = _img
+
+    # Freeze model
+    for p in model.parameters():
+        p.requires_grad = False
+
+    # Move model to gpu and eval mode
     model.to('cuda')
     model.eval()
-    # img = Image.open('castoro.jpg')
-    # #img = img.resize((224, 224))
-    # img = transforms.ToTensor()(img)
-    img = _img.unsqueeze(0)
-    img = img.to('cuda')
 
-    features = model(img)
-    
+    # Extract features
+    features = model(img.unsqueeze(0).to('cuda'))
+
     # Moves images and model to cpu
     img = img.cpu()
     model = model.cpu()
-    all_layers_features = [x.cpu().numpy() for x in features]
+    all_layers_features = [x.cpu() for x in features]
+    
+    # Save feature maps + image
+    with open(f'{fname}.pkl', 'wb') as f:
+        pkl.dump(all_layers_features+[img], f)
 
-    prefix = f'fmaps/{model_name}/pretrained_{pretrained}'
-    if not os.path.exists(prefix):
-        os.makedirs(prefix)
-    return save_fmaps2(img, all_layers_features, prefix)
+    #return all_layers_features, img
+    return [x.shape[1] for x in all_layers_features]
 
-def save_fmaps2(img, all_layers_features, prefix):
+
+@st.cache_data
+def create_plots(fname, num_fmaps):
+    """Create feature map plots
+    Args:
+        fname (str): filename of feature maps
+        num_fmaps (list): numbers of feature maps to plot for each layer
+    Returns:
+        None
+    """
+    prefix = fname.split('/')[0]
+
+    # Load feature maps
+    with open(f'{fname}.pkl', 'rb') as f:
+        restore = pkl.load(f)
+
+    # Extract features and image
+    all_layers_features = restore[:-1]
+    img = restore[-1]
+    
+    # normalize image to [0,255]
     h,w = img.shape[-2], img.shape[-1]
     img = img.squeeze(0).permute(1,2,0).numpy()
-    img = img - img.min()
-    img = img / img.max() * 255
-    img = img.astype(np.uint8)
-    
+    img = to_255(img).astype(np.uint8)
+
+    # Save plots
     fnames = []
-    #import ipdb; ipdb.set_trace()
     for i, features_layer_i in enumerate(all_layers_features):
-        for j, feature_j in enumerate(features_layer_i[0][:35]):
-            feature_j = cv2.resize(feature_j, (w,h))
-            feature_j = feature_j - feature_j.min()
-            feature_j = feature_j / feature_j.max() * 255
-            feature_j = feature_j.astype(np.uint8)
-            feature_j = cv2.applyColorMap(feature_j, cv2.COLORMAP_JET)
-            heatmap = cv2.addWeighted(img, 0.45, feature_j, 0.55, 0)
+        features_layer_i = features_layer_i.numpy()
+        for j, feat_j in enumerate(features_layer_i[0][:num_fmaps[i]]):
+            feat_j = cv2.resize(feat_j, (w,h))
+            # normalize feature map to [0,255]
+            feat_j = to_255(feat_j).astype(np.uint8)
+            #feat_j = cv2.applyColorMap(feat_j, cv2.COLORMAP_JET)
+            feat_j = cv2.applyColorMap(feat_j, cmapy.cmap('coolwarm'))
+            heatmap = cv2.addWeighted(img, 0.35, feat_j, 0.65, 0)
             fname = f'{prefix}/{i:03}_{j:03}.jpg'
             cv2.imwrite(fname, heatmap)
             fnames.append(fname)
     return fnames
 
-def save_fmaps(img, all_layers_features, pretrained):
-   # Save feature maps
-    fnames = []
-    for i, features_layer_i in enumerate(all_layers_features):
-        for j, feature_j in enumerate(features_layer_i[0][:35]):
-            feature_j = feature_j.unsqueeze(0).unsqueeze(0)
-            h,w = img.shape[-2], img.shape[-1]
-            fname = f'fmaps/{model_name}/pretrained_{pretrained}/{i:03}_{j:03}.jpg'
-            feature_j = F.interpolate(feature_j, size=[h,w], mode='bicubic')
-            #feature_j = feature_j.expand(1,3, -1, -1)
-            print(feature_j.dtype, img.dtype)
-            print(feature_j.shape, img.shape)
-            print(feature_j.min(), feature_j.max(), img.min(), img.max())
-            print(img[0].dtype, type(img[0]))
-            ts.overlay([img[0], feature_j[0]], cmap='coolwarm', save_as=fname, alpha=[1, 0.8])
-            #ts.save(feature_j, path=fname, cmap='coolwarm')
-            fnames.append(fname)
-    return fnames
 
 @st.cache_data
 def load_model(model_name, pretrained):
+    """Load model
+    Args:
+        model_name (str): model name
+        pretrained (bool): load pretrained weights
+    Returns:
+        model (torch.nn.Module): model
+        model_is_supported (bool): model is supported
+    """
     try:
         return timm.create_model(model_name, pretrained=pretrained, features_only=True), True
     except Exception as e:
@@ -100,38 +124,16 @@ def load_model(model_name, pretrained):
         return None, False
 
 
-def show_filters(tensor, selected_module):
-    if len(tensor.shape) == 4 and (tensor.shape[-1] == tensor.shape[-2] and tensor.shape[-1] == 1):
-        ts.save(tensor.squeeze(-1).squeeze(-1), 'imgs/tensor.png')
-        st.image('imgs/tensor.png', width=300, caption=selected_module)
-    elif  len(tensor.shape) == 4 and tensor.shape[0] > 100:
-        tensor = tensor[:12, :32, :, :] 
-        #tensor = rearrange(tensor, 'f c h w -> c h (w f)')
-        ts.save(tensor, 'imgs/tensor.png')
-        st.image('imgs/tensor.png', width=300, caption=selected_module + ' (first 10 features, first 32 channels stacked)')
-    else:
-        
-        # ch = st.slider('Next Channels:', min_value=1, max_value=10, value=1)
-        # start = (ch-1)*3
-        # end = ch*3
-        # ts.save(tensor[:64, :end, :, :], 'imgs/tensor.png')
-        ts.save(tensor, 'imgs/tensor.png')
-        st.image('imgs/tensor.png', width=300, caption=selected_module)
-
-
+###############################################################################
 st.set_page_config(layout="wide")
 
-
-
-
-###############################################################################
-###############################################################################
-###############################################################################
 # Global variables
 if 'pretrained' not in st.session_state:
     st.session_state.pretrained = False
 if 'logs' not in st.session_state:
     st.session_state.logs = []
+if 'img_file_buffer' not in st.session_state:
+    st.session_state.img_file_buffer = []
 ###############################################################################
 
 
@@ -146,8 +148,8 @@ with st.sidebar:
         st.table(model_size(model))
 
 
-    
-    tog.st_toggle_switch(label="Pretrained", 
+
+    _ = tog.st_toggle_switch(label="Pretrained", 
                         key="pretrained", 
                         default_value=False, 
                         label_after = False, 
@@ -155,91 +157,69 @@ with st.sidebar:
                         active_color="#11567f", 
                         track_color="#29B5E8"
                         )
-    
+
 
 
 if not model_is_supported:
-    st.markdown('# Something wrong :/')
-    st.markdown(f'`More Logs : {st.session_state.logs[-1]}`')
+    st.markdown(f'# {st.session_state.logs[-1]}')
+
 else:
-    # disable gradient and create df
-    module_list = []
-    for name, param in model.named_parameters():
-        param.requires_grad = False
-        module_list.append({'module': name, 'shape': param.shape})
-        df = pd.DataFrame.from_records(module_list)
-        df.columns=['module', 'shape']
-
-    with st.sidebar:
-        st.markdown("## Parameters")
-        gb = GridOptionsBuilder.from_dataframe(df[["module", "shape"]])
-        # configure selection
-        gb.configure_selection(selection_mode="single", use_checkbox=True)
-        gb.configure_side_bar()
-        gridOptions = gb.build()
-        data = AgGrid(df,
-                    gridOptions=gridOptions,
-                    enable_enterprise_modules=True,
-                    allow_unsafe_jscode=True,
-                    height=500,
-                    update_mode=GridUpdateMode.SELECTION_CHANGED,
-                    columns_auto_size_mode=ColumnsAutoSizeMode.FIT_ALL_COLUMNS_TO_VIEW)
-
-
-    # Default option is the first layer.
-    if len(data["selected_rows"]) != 0:
-        selected_row = data["selected_rows"][0]
-    else:
-        selected_row = df.iloc[0]
-    selected_module = selected_row["module"]
-    selected_shape = selected_row["shape"]
-
-    # Get the selected tensor
-    attrs = selected_module.split(".")
-    tensor = process_attrs(model, attrs)
-
-    # adds batch size of 1 if no bsize is present
-    if len(tensor.shape) == 1:
-        tensor = tensor.unsqueeze(0)
-
-
-
 
     col1, col2 = st.columns(2)
     with col1:
         #show_filters(tensor, selected_module)
         # File uploader
         st.markdown(f"## Upload picture")
-        img_file_buffer = st.file_uploader('upload your image')
-        if img_file_buffer is not None:
-            st.image(img_file_buffer, width=300, caption='Uploaded Image')  
- 
-        show_filters(tensor, selected_module)
-
-
+        img_file_buffer = st.file_uploader('')
+        if img_file_buffer:
+            st.session_state.img_file_buffer = img_file_buffer
 
     with col2:
-        st.markdown(f"### .{selected_module} {tuple(selected_shape)}")
-        #nbins = st.slider('Histogram bins:', min_value=5, max_value=200, value=100)
-        show_hist(tensor, nbins=100)
-        st.image('imgs/hist.png', width=400, caption=f'Histogram of all the module weights')
+        if st.session_state.img_file_buffer != []:
+            st.image(st.session_state.img_file_buffer, width=300, caption='Uploaded Image')  
 
-                         
-    # Feature map visualization
-    if img_file_buffer is not None:
-        PIL_image = Image.open(img_file_buffer)
-        tensor_image = transform.to_tensor(PIL_image)
-        image_files = prepare_feature_maps(model, model_name, tensor_image, img_file_buffer.name, pretrained=st.session_state.pretrained)
+
+    # Activation map visualization
+    if st.session_state.img_file_buffer != []:
         
-        print(st.session_state.pretrained)
-        with st.container():
-            st.write("## Feature maps")
-            images = []
-            image_captions = []
-            for i, image_path in enumerate(image_files):
-                images.append(open(image_path, 'rb').read())
-                layer = image_path.split('/')[-1].split('_')[0]
-                feature = image_path.split('/')[-1].split('_')[1].split('.')[0]
-                image_captions.append(f'{int(layer)}.{int(feature)}')
-            st.image(images,width=200,caption=image_captions)
+        tensor_image = transform.to_tensor(Image.open(st.session_state.img_file_buffer))
 
+        prefix = f'fmaps/{model_name}/pretrained_{st.session_state.pretrained}'
+        if not os.path.exists(prefix):
+            os.makedirs(prefix)
+        
+        fname=f'{prefix}/{st.session_state.img_file_buffer.name.split(".")[0]}'
+        
+        try:
+            len_fmaps = prepare_feature_maps(model, tensor_image, fname)
+
+            # slider to select number of feature maps
+            with st.sidebar:
+                num_fmaps = []
+                for i in range(len(len_fmaps)):
+                    number = st.number_input(f'Number of activation maps in layer {i+1}', 1, len_fmaps[i], 10)
+                    num_fmaps.append(number)
+
+            image_files = create_plots(fname, num_fmaps)
+
+            with st.container():
+                st.write("## Activation maps")
+                img_dict = defaultdict(list)
+
+                for i, image_path in enumerate(image_files):
+                    layer = image_path.split('/')[-1].split('_')[0]
+                    img_dict[layer].append(open(image_path, 'rb').read())
+                    #feature = image_path.split('/')[-1].split('_')[1].split('.')[0]
+                    #image_captions.append(f'{int(layer)}.{int(feature)}')
+
+                for k in img_dict.keys():
+                    st.write(f'### Layer {int(k)+1}')
+                    images = img_dict[k]
+                    image_captions = [f'{i}' for i in range(len(images))]
+                    st.image(images,width=200,caption=image_captions)
+
+        except Exception as e:
+            st.session_state.logs.append("The forward failed for some reason. Check your console!")
+            st.markdown(f"# {st.session_state.logs[-1]}")
+            print(e)
+            st.stop()
